f976d49b178ebf4bfe77000144c4796b14c8243b
2br-dev <masterkadaj@gmail.com>
2020-12-28 18:35:01 +0300

    Navi_top

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,36 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+
 var browserSync = require('browser-sync').init({
-    server: {
-        baseDir: './'
-    }
+	server: {
+		baseDir: './'
+	}
 });
 
 gulp.task('sass', function() {
-    return gulp.src('./src/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./css'))
-        .pipe(browserSync.stream())
+	return gulp.src('./src/scss/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('./css'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('html', function(){
+	return gulp.src('./*.html')
+		.pipe(browserSync.stream());
+});
+
+gulp.task('js', function(){
+	return gulp.src('./js/*.js')
+		.pipe(browserSync.stream());
 });
 
 gulp.task('browser-sync', function(){
-    return browserSync.reload();
+	return browserSync.reload();
 });
 
 gulp.task('watch', function(){
-    gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
-    gulp.watch('./*.html', gulp.series('browser-sync'));
-    gulp.watch('./js/*.js', gulp.series('browser-sync'));
+	gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+	gulp.watch('./*.html', gulp.series('html'));
+	gulp.watch('./js/*.js', gulp.series('js'));
 });
