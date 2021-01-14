017c8a69f78e86838adb9cb46bb9541079f8a17e
2br-dev <masterkadaj@gmail.com>
2021-01-14 14:35:44 +0300

    Все, кроме новостей и поддержки

--- js/data_loader.js
+++ js/data_loader.js
@@ -1,71 +1,84 @@
-var product_card_tpl = `
-<div class="card-wrapper swiper-slide">
-    <div class="card product-card">
-        <div class="card-content">
-            <div class="lr-pair">
-                <div class="left">
-                    <div class="lazy-image product-image contain" data-src="/img/mirabelle.png"></div>
+var product_card_tpl=`
+<div class="card product-card">
+    <div class="card-content">
+        <div class="header">«Долина Джентау» в&nbsp;одноразовой таре</div>
+        <div class="lr-pair">
+            <div class="left">
+                <a href="/product.html" class="lazy-image product-image contain" data-src="/img/mirabelle.png"></a>
+            </div>
+            <div class="right grow">
+                <div class="sub-header">Вода питьевая</div>
+                <div class="lr-pair table-header">
+                    <div class="left">Бутылок</div>
+                    <div class="separator grow"></div>
+                    <div class="right">Цена</div>
                 </div>
-                <div class="right grow">
-                    <div class="header">«Мирабель»</div>
-                    <div class="sub-header">Вода питьевая</div>
-                    <div class="lr-pair table-header">
-                        <div class="left">Бутылок</div>
-                        <div class="separator grow"></div>
-                        <div class="right">Цена</div>
-                    </div>
-                    <div class="lr-pair dotted">
-                        <div class="left count">1</div>
-                        <div class="separator grow"></div>
-                        <div class="right price">130₽</div>
-                    </div>
-                    <div class="lr-pair dotted">
-                        <div class="left count">от 2х</div>
-                        <div class="separator grow"></div>
-                        <div class="right price">120₽</div>
-                    </div>
-                    <div class="lr-pair dotted">
-                        <div class="left count">от 4х</div>
-                        <div class="separator grow"></div>
-                        <div class="right price">110₽</div>
-                    </div>
+                <div class="lr-pair dotted">
+                    <div class="left count">1</div>
+                    <div class="separator grow"></div>
+                    <div class="right price">130₽</div>
+                </div>
+                <div class="lr-pair dotted">
+                    <div class="left count">от 2х</div>
+                    <div class="separator grow"></div>
+                    <div class="right price">120₽</div>
+                </div>
+                <div class="lr-pair dotted">
+                    <div class="left count">от 4х</div>
+                    <div class="separator grow"></div>
+                    <div class="right price">110₽</div>
                 </div>
             </div>
         </div>
-        <form class="card-footer">
-            <div class="lr-pair">
-                <div class="left grow">
-                    <div class="add-cart">
-                        <div class="plus">
-                            <a href="">–</a>
-                        </div>
-                        <div class="input" contenteditable="true">0</div>
-                        <div class="minus">
-                            <a href="">+</a>
-                        </div>
-                    </div>
-                    <input type="hidden" name="product_id" value="1" />
-                    <input type="hidden" name="count" />
-                </div>
-                <div class="right">
-                    <a href="javascript:void(0);" class="btn btn-flat waves-effect waves-red">Купить</a>
-                </div>
-            </div>
-        </form>
     </div>
+    <form class="card-footer">
+        <div class="lr-pair">
+            <div class="left grow">
+                <div class="add-cart">
+                    <div class="plus">
+                        <a href="">–</a>
+                    </div>
+                    <div class="input" contenteditable="true">0</div>
+                    <div class="minus">
+                        <a href="">+</a>
+                    </div>
+                </div>
+                <input type="hidden" name="product_id" value="1" />
+                <input type="hidden" name="count" />
+            </div>
+            <div class="right">
+                <a href="javascript:void(0);" class="btn btn-flat waves-effect waves-dark">Купить</a>
+            </div>
+        </div>
+    </form>
+</div>
+`
+
+var slider_card_tpl = `
+<div class="card-wrapper swiper-slide">
+`+product_card_tpl+`
 </div>
 `;
 
+var col_card_tpl = `
+<div class="col xl3 l4 m6 s12">
+`+product_card_tpl+`
+</div>
+`
+
 $(() => {
     fillSlider();
+    fillProducts();
 })
 
 function fillSlider(){
     for(f=0; f<=14; f++){
-        $('#misc-products-container').append(product_card_tpl);
+        $('#misc-products-container').append(slider_card_tpl);
     }
 }
 
-$(() => {
-    
-})
+function fillProducts(){
+    for(f=0; f<24; f++){
+        $('#products-wrapper').append(col_card_tpl);
+    }
+}
--- js/master.js
+++ js/master.js
@@ -1,25 +1,160 @@
 var heroSlider;
+var heroSliderMobile;
 var miscSlider;
+var observe;
 
 $(() => {
 
     initSliders();
-    loadImages()
+    loadImages();
+
+    $('body').on('click', '.sidenav .folder', nextLevel);
+    $('body').on('click', '.sidenav .back', prevLevel);
+    $('body').on('change', '#change-password-trigger', showChangePass);
+    $('body').on('change', '.set-password-trigger', showSetPass);
+    $('body').on('keyup', 'textarea', updateTextarea);
+    $('body').on('click', '.dropdown', openBranchPopup);
+    $('body').on('click', '.dropdown a', setCurrent);
+    $('body').on('click', 'tr.order', toggleDetails);
+    $('body').on('click', hidePopups);
+    $('body').on('click', 'tr.details', closeDetails);
+    $('body').on('change', '[name="theme-selector"]', disenTheme);
+
+    $('.sidenav').sidenav();
+    $('.modal').modal();
+    $('p').hyphenate();
+    $('.material-table').materialize();
 });
+
+function updateTextarea(){
+    this.style.height = 0;
+    this.style.height = (this.scrollHeight - 7) + 'px';
+}
+
+
+function disenTheme(){
+    switch(this.id){
+        case "theme-selector1": $('#theme-selector').addClass('disabled'); break;
+        case "theme-selector2": $('#theme-selector').removeClass('disabled'); break;
+    }
+}
+
+function showSetPass(){
+
+    $('#set-password').toggleClass('visible');
+}
+
+function showChangePass(){
+    
+    $('#change-password').toggleClass('visible');
+}
+
+function toggleDetails(e){
+
+    var already = $(this).hasClass('expanded');
+    var newClass = already ? '' : 'expanded'
+    var path = e.originalEvent.path;
+
+    var link_in_path = path.filter(el => {
+        return el.tagName == 'A'
+    });
+
+    console.log(link_in_path);
+    if(!link_in_path.length){
+        $('tr.order').removeClass('expanded');    
+        $(this).addClass(newClass);
+    }
+}
+
+function closeDetails(e){
+
+    if($('html').outerWidth() <= 600){
+
+        $(this).prev().removeClass('expanded');
+    }
+}
+
+function setCurrent(e){
+    e?.preventDefault();
+    var newVal = $(this).text();
+    $(this).parents('.dropdown-wrapper').find('.current').text(newVal);
+}
+
+function hidePopups(e){
+
+    var path = e.originalEvent.path;
+    var dropdownIndex = path.filter(el => {
+        return $(el).hasClass('popup');
+    })
+
+    if(!dropdownIndex.length){
+        $('.dropdown-wrapper .popup').removeClass('open');
+    }
+}
+
+function openBranchPopup(e){
+    e?.preventDefault();
+    e?.stopPropagation();
+    $(this).find('.popup').toggleClass('open');
+}
+
+function prevLevel(e){
+    e?.preventDefault();
+    var oldSL = $('.main-catalog-scroller').scrollLeft();
+    var sbW = $('.sidenav').outerWidth();
+    var newSL = oldSL-sbW;
+    var subMenu = $(this).closest('ul');
+
+    $('.main-catalog-scroller').animate({
+        scrollLeft: newSL
+    }, () => {
+        subMenu.removeClass('visible');
+    });
+}
+
+function nextLevel(e){
+    e?.preventDefault();
+
+    var oldSL = $('.main-catalog-scroller').scrollLeft();
+    var sbW = $('.sidenav').outerWidth();
+    var newSL = oldSL+sbW;
+    var subMenu = $(this).next();
+
+    subMenu.addClass('visible');
+    
+    $('.main-catalog-scroller').animate({
+        scrollLeft: newSL,
+        scrollTop: 0
+    });
+}
 
 function initSliders(){
 
     if($('#hero-slider').length){
         heroSlider = new Swiper('#hero-slider', {
-            
+            effect: "fade",
+            loop: true,
+            autoplay: {
+                delay: 7000
+            }
+        });
+    }
+
+    if($('#hero-slider-mobile').length){
+        heroSliderMobile = new Swiper('#hero-slider-mobile', {
+            loop: true,
+            autoplay: {
+                delay: 7000
+            }
         });
     }
 
     if($('#misc-products').length){
         miscSlider = new Swiper('#misc-products', {
             autoplay: {
-               delay: 7000
+               delay: 5000
             },
+            loop: true,
             breakpoints: {
                 1600: {
                     slidesPerView: 4
@@ -46,6 +181,7 @@ function openImages(){
 
 function loadImages(){
     $('.lazy-image').each((index, el) => {
+
         var url = $(el).data('src');
         // debugger
 
@@ -55,5 +191,26 @@ function loadImages(){
         var imgTag = $('<img src="'+url+'" class="lazy-loading" />')[0];
         $(el).append(imgTag);
         $('img').one('load', openImages);
+    })
+}
+
+$.fn.materialize = function(){
+
+    return this.each(function() {
+        
+        var table_columns = $(this).find('th');
+        var table_rows = $(this).find('tr');
+        var column_names = [];
+
+        $(table_columns).each((index, col) => {
+            column_names.push(col.innerText)
+        });
+
+        $(table_rows).each((index, row) => {
+
+            $(row).find('td').each((index, cell) => {
+                $(cell).attr('data-prefix', column_names[index]);
+            })
+        });
     })
 }
